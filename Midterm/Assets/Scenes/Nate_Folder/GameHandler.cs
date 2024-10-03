9564e1499217a269ca3f0be731eb290177b87639
nperry02 <nate@atperry.com>
2024-10-03 19:56:19 -0400

    Created the start of the lava level

---
+++ Midterm/Assets/Scenes/Nate_Folder/GameHandler.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameHandler : MonoBehaviour {
+
+      private GameObject player;
+      public static int playerHealth = 100;
+      public int StartPlayerHealth = 100;
+      public GameObject healthText;
+
+      public bool isDefending = false;
+
+      public static bool stairCaseUnlocked = false;
+      //this is a flag check. Add to other scripts: GameHandler.stairCaseUnlocked = true;
+
+      private string sceneName;
+      public static string lastLevelDied;  //allows replaying the Level where you died
+
+      void Start(){
+            player = GameObject.FindWithTag("Player");
+            sceneName = SceneManager.GetActiveScene().name;
+            //if (sceneName=="MainMenu"){ //uncomment these two lines when the MainMenu exists
+                  playerHealth = StartPlayerHealth;
+            //}
+            updateStatsDisplay();
+      }
+
+
+      public void playerGetHit(int damage){
+           if (isDefending == false){
+                  playerHealth -= damage;
+                  if (playerHealth >=0){
+                        updateStatsDisplay();
+                  }
+            }
+
+           if (playerHealth > StartPlayerHealth){
+                  playerHealth = StartPlayerHealth;
+                  updateStatsDisplay();
+            }
+
+           if (playerHealth <= 0){
+                  playerHealth = 0;
+                  updateStatsDisplay();
+                  //playerDies();
+            }
+      }
+
+      public void updateStatsDisplay(){
+            Text healthTextTemp = healthText.GetComponent<Text>();
+            healthTextTemp.text = "HEALTH: " + playerHealth;
+      }
+
+}
