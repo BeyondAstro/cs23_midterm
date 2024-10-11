365a050c92f4767280f02b7e8a0354984f6fd98e
nperry02 <nate@atperry.com>
2024-10-11 14:36:41 -0400

    Pressure Plates Changed with crates

---
+++ Midterm/Assets/Scenes/Nate_Folder/PressurePlateActivate.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlateActivate : MonoBehaviour
+{
+    public float successDistanceThreshold = 1f;
+    public GameObject[] objectivePoints;
+    public GameObject[] objectives;
+    public bool objectiveComplete = false;
+    public GameObject blocker;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Start()
+    {
+        blocker.SetActive(true);
+    }
+    void Update()
+    {
+        if (objectiveComplete == false && CheckObjectiveCompletion())
+        {
+            blocker.SetActive(false);
+            objectiveComplete = true;
+        }
+    }
+
+    private bool CheckObjectiveCompletion()
+    {
+        for (int i = 0; i < objectives.Length; i++)
+        {
+
+            float distance = Vector3.Distance(objectivePoints[i].transform.position, objectives[i].transform.position);
+            Debug.Log($"Checking objective {i}: Distance = {distance}");
+
+            if (Vector3.Distance(objectivePoints[i].transform.position, objectives[i].transform.position) >= successDistanceThreshold)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
