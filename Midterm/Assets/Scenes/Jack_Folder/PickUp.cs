40059e9e99996f650ad8ce851e01da82079da535
Jack-86 <jack.l008600@gmail.com>
2024-10-13 20:26:02 -0400

    Tried to fix scene change
    Tried to add the final you win screen without success

--- Midterm/Assets/Scenes/Jack_Folder/PickUp.cs
+++ Midterm/Assets/Scenes/Jack_Folder/PickUp.cs
@@ -5,12 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class PickUp : MonoBehaviour
 {
-    public string name = "ToBeContinued";
+    private static string name = "Scenes/ToBeContinued";
 
     // Update is called once per frame
     void OnCollisionEnter2D(Collision2D c)
     {
         Debug.Log("Collision Detected");
+        Debug.Log(name);
         SceneManager.LoadScene(SceneManager.GetSceneByName(name).buildIndex);
+        Debug.Log(name);
     }
 }
